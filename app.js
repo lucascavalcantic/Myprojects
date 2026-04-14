9788a4ecfe5cb730ef48862b34bc97bd2fac7e5c
lucascavalcantic <lucas.cavalcanti@ibft.com.br>
2026-04-14 09:55:08 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+miro.onReady(async () => {
+  await miro.board.ui.on('icon:click', async () => {
+    await miro.board.ui.openPanel({
+      url: 'panel.html',
+      width: 620,
+    });
+  });
+});
